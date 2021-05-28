e5d1828c46e8fa34febd97c8d519b72fa40ef14e
ngduc <nducus@yahoo.com>
2021-05-28 18:42:24 +0100

    add fetch fn to call a dummy api

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 interface AppProps {}
 
 function App({}: AppProps) {
+  const [content, setContent] = React.useState('')
   // Create the count state.
   // const [count, setCount] = useState(0);
   // Create the counter (+1 every second).
@@ -12,6 +13,19 @@ function App({}: AppProps) {
   //   const timer = setTimeout(() => setCount(count + 1), 1000);
   //   return () => clearTimeout(timer);
   // }, [count, setCount]);
+
+  const onPublishClick = async () => {
+    const res = await fetch('http://209.182.218.3:5000/api/publish', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: content
+      }),
+    })
+    console.log('res', res)
+  }
 
   // Return the App component.
   return (
@@ -23,7 +37,11 @@ function App({}: AppProps) {
         </p>
 
         <p>
-          Content
+          <button onClick={onPublishClick} className="p-2 border-gray-100 border rounded text-sm bg-gray-700 hover:bg-gray-600">Publish This Content</button>
+        </p>
+
+        <p className="w-full">
+          <textarea defaultValue="Hello World" onChange={(ev) => setContent(ev.target.value)} className="text-sm text-gray-700 p-2" />
         </p>
         
       </header>
