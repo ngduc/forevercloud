27ed73dbaca37cc05490e6da922245f75fc3483f
ngduc <nducus@yahoo.com>
2021-05-28 17:39:28 +0100

    feat: initial snowpack ui

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# web3-thu-duc
+# Web3
---
+++ src/ui/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  // Create the count state.
+  // const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  // useEffect(() => {
+  //   const timer = setTimeout(() => setCount(count + 1), 1000);
+  //   return () => clearTimeout(timer);
+  // }, [count, setCount]);
+
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          Web3
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
